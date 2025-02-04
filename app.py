ba5778f8a8736ed1850ac3b2f207ca0b065ecec7
sajid khan <sajidk25@gmail.com>
2025-02-05 03:33:00 +0600

    app.config session

--- app.py
+++ app.py
@@ -20,6 +20,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config['SECRET_KEY'] = 'sample_secret_key_123456'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
 # SMTP Server Config
 app.config['MAIL_SERVER'] = 'smtp.example.com'  # Replace with your SMTP server
 app.config['MAIL_PORT'] = 587
