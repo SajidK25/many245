f526ce407480112bfa02a61b4eb708ba86918d06
sajid khan <sajidk25@gmail.com>
2025-04-05 01:39:50 +0600

    No data image added and driver on time notification added

--- models.py
+++ models.py
@@ -85,7 +85,7 @@ class UserNotification(db.Model):
 
     __table_args__ = (
         db.CheckConstraint(
-            "category IN ('Driver Late', 'Bob Tail', 'Ready to Pickup', 'Pink Notification')",
+            "category IN ('Driver Late', 'Bob Tail', 'Ready to Pickup', 'Driver on time')",
             name="check_category_valid"
         ),
     )
