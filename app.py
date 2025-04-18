4c727a083d169ab6fb68ce5b5fd06ff8155d7825
sajid khan <sajidk25@gmail.com>
2025-04-18 23:32:08 +0600

    modified:   app.py

--- app.py
+++ app.py
@@ -850,7 +850,7 @@ def scrape_amazon_relay():
         "-v", f"{output_path}:/tmp/user_profile",
         "-e", f"RELAYUSERNAME={user.amazon_relay_email}",
         "-e", f"RELAYPASSWORD={user.amazon_relay_password}",
-        "mg_docker_relay_pull"
+        "mg_docker_relay_pull:latest"
     ]
 
     try:
